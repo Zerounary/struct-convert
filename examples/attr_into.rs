6529fa3c2c9578471a8c2f6008a6389894cf42a3
Zerounary <850657663@qq.com>
2022-09-07 12:59:36 +0800

    example add inner case

--- examples/attr_into.rs
+++ examples/attr_into.rs
@@ -1,19 +1,21 @@
-use struct_convert::{Convert};
+use struct_convert::Convert;
 
-#[derive(Debug,Default, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
+struct BInner {
+    name: String,
+}
+
+#[derive(Debug, Default, PartialEq)]
 struct B {
     bid: i64,
     num: String,
     name: String,
+    inner: BInner,
 }
 
-#[derive(Debug,Default, Convert, PartialEq)]
+#[derive(Debug, Default, Convert, PartialEq)]
 #[convert_into(into = "B")]
-// #[attr_into(into = "a::BVo", include("a as b"))] // 别名
-// #[attr_into(into = "a::BVo", include("a.to_string()"))] // 直接手动指名
-// #[attr_into(into = "a::BVo", exclude("a.to_string()"))] // 直接手动指名
 struct A {
-
     #[convert_field(rename = "bid")]
     id: i64,
 
@@ -21,12 +23,36 @@ struct A {
     num: i64,
 
     #[convert_field(unwrap)]
-    name: Option<String>
-    
+    name: Option<String>,
+
+    inner: AInner,
+}
+
+#[derive(Debug, Default, Convert, PartialEq)]
+#[convert_into(into = "BInner")]
+struct AInner {
+    name: String,
 }
 
 fn main() {
-    let a = A { id: 2, num: 1, name: Some("Jack".to_string()) };
+    let a = A {
+        id: 2,
+        num: 1,
+        name: Some("Jack".to_string()),
+        inner: AInner {
+            name: String::from("AInner"),
+        },
+    };
     let b: B = a.into();
-    debug_assert_eq!(B {num: "1".to_string(), bid: 2, name: "Jack".to_string() }, b);
+    debug_assert_eq!(
+        B {
+            num: "1".to_string(),
+            bid: 2,
+            name: "Jack".to_string(),
+            inner: BInner {
+                name: String::from("AInner")
+            }
+        },
+        b
+    );
 }
