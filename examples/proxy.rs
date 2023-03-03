140d139f95de2318c87683d9b2bae3e27a7cb096
Zerounary <song.c@sungeon.com>
2023-03-03 11:15:13 +0800

    fix: remove log code

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -1,6 +1,9 @@
 use struct_convert::Convert;
 
+use crate::some_mod::Remote;
+
 #[derive(Default, Convert)]
+#[convert(from = "Remote")]
 #[convert(from = "C")]
 #[convert(into = "B")]
 struct A {
@@ -15,9 +18,19 @@ struct C {
     value: i64,
 }
 
+mod some_mod {
+    pub struct Remote {
+        pub value: i64,
+    }
+}
+
 fn main() {
     let c = C { value: 8 };
     let a: A = c.into();
     let b: B = a.into();
     debug_assert_eq!(8, b.value);
+
+    let r = Remote{value: 7};
+    let a2: A = r.into();
+    debug_assert_eq!(7, a2.value);
 }
